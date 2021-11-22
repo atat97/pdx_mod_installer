de114c7feb011f4a73b337efbe2b0cdfe3f2397b
atat97 <a_tatusko@wp.pl>
2021-11-22 16:50:27 +0100

    Added comments; Simplified condition in the pdx_install function; Removed redundant comments

--- pdx_mod_installer.py
+++ pdx_mod_installer.py
@@ -7,18 +7,20 @@
 import os
 import zipfile
 
+#Function that appends the "path" and "name" lines at the begginig of a file
 def preappend_line(file_name, line):
     dummy = file_name + ".mod"
     with open(file_name, 'r') as read_obj, open(dummy, 'w') as write_obj:
         write_obj.write(line)
         for line in read_obj:
+            #Don't write the old "name" and "path" lines:
             if ("path" not in line) and ("name" not in line) :
                 write_obj.write(line)
     os.remove(file_name)
     os.rename(dummy,file_name)
 
 def pdx_install(path):
-    directory = os.fsencode(path) if os.path.exists(path) == True else print("Invalid path")
+    directory = os.fsencode(path) if os.path.exists(path) else print("Invalid path")
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".zip"): #TODO: Works only with .zip extension            
@@ -30,18 +32,16 @@ def pdx_install(path):
             #Delete the archive
             #os.remove(path + "/" + filename) # Optional
             
-            #Extract descriptor #TODO: Specifically checks for "descriptor.mod"/Should check for "*.mod"
+            #Extract descriptor
             for file in os.listdir(path + "/" + folder_name):
                 if os.fsdecode(file).endswith(".mod"):
                     desc = os.fsdecode(file)
             os.replace(path + "/" + folder_name + "/" + desc, path + "/" + folder_name + ".mod")
-            #Strip the existing path and name line
             desc_path = path + "/" + folder_name + ".mod"
             
             #Add the path and name lines to the descriptor
             preappend_line(desc_path, "path=\"" + path + "/" + folder_name + "\"\n" + "name=\"" + folder_name + "\"\n")
             print("Finitto!")
 
-#pdx_install("C:/Users/A/Documents/Paradox Interactive/Test/mod")
 pdx_install(input("Enter the path to the mod folder (ex. ../mod): ").replace("\\","/"))
 input("Press enter to exit...")
